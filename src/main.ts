3dd30933435d455024fa4b27b7760b6cddf5e116
DuckBoy <a-ten2543@hotmail.com>
2022-01-01 23:34:28 +0700

    NavBar

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 
+
+
 import App from './App.vue'
 import Router from './router'
+
 
 createApp(App).use(Router).mount('#app')
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,2 +1,45 @@
-<template src="./home.html"></template>
-<script lang="ts" src="./home.ts"></script>
+<template>
+  <section class="Room Occupy">
+    <div class="Room">
+      <div class="container">
+        <form class="box" >
+          <h1> Single Room </h1>
+          <p> A room assigned to one person.
+            The room size or area of Single Rooms are generally between 37 m² to 45 m². </p>
+        </form>
+        <form class="box" >
+          <h1> Double Room </h1>
+          <p> A room assigned to two people.
+          The room size or area of Double Rooms are generally between 40 m² to 45 m². </p>
+        </form>
+        <form class="box" >
+          <h1> Quad Room </h1>
+          A room assigned to four people.
+            The room size or area of Quad Rooms are generally between 70 m² to 85 m².
+        </form>
+        <form class="box" >
+          <h1> Deluxe Room </h1>
+          <p> A room assigned for one or two people. The room size or area of Single Rooms are generally between 37 m² to 45 m². </p>
+        </form>
+        <form class="box" >
+          <h1> VIP Room </h1>
+          <p> A room assigned for two to four people. The room size or area of Single Rooms are generally between 40 m² to 85 m². </p>
+        </form>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+  .box {
+    display: block;
+    background-color: lightgrey;
+    width: 750px;
+    border: 5px solid green;
+    padding: 40px;
+    margin: 20px;
+    }
+  .h1 {
+    text-align:center;
+  }
+</style>
